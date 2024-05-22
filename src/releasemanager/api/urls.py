dd8b25df0185f757fafafadc74e108fa7ccab07a
Grant Viklund <grantv@blackmesaventures.com>
2024-05-21 20:26:51 -0400

    removed the extra "api" from the URL

--- src/releasemanager/api/urls.py
+++ src/releasemanager/api/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from .views import ReleaseListView, ReleaseCreateView, ReleaseFileUpdateView
 
 urlpatterns = [
-    path("api/releases/", ReleaseListView.as_view(), name="api_releases"),
-    path("api/releases/new/", ReleaseCreateView.as_view(), name="api_create_release"),
+    path("releases/", ReleaseListView.as_view(), name="api_releases"),
+    path("releases/new/", ReleaseCreateView.as_view(), name="api_create_release"),
     path(
-        "api/releases/<int:pk>/update_files/",
+        "releases/<int:pk>/update_files/",
         ReleaseFileUpdateView.as_view(),
         name="api_update_files",
     ),
