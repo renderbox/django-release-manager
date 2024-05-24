976fbadf5e1fa8e2457fb165944da9c699c27587
Grant Viklund <grantv@blackmesaventures.com>
2024-05-24 14:05:28 -0400

    added API test for creating new release of a package

--- src/releasemanager/tests.py
+++ src/releasemanager/tests.py
@@ -1,14 +1,18 @@
 from django.test import TestCase
-from django.urls import reverse
+
+# from django.urls import reverse
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import Group, Permission
-from django.conf import settings
+
+# from django.contrib.auth.models import Group, Permission
+# from django.conf import settings
 
 from django.contrib.sites.models import Site
-from .models import Release, Status
-from django.utils import timezone
 
-from rest_framework.test import APIClient
+from .models import Release, Status
+
+# from django.utils import timezone
+
+# from rest_framework.test import APIClient
 from rest_framework import status
 from rest_framework.test import APITestCase
 
@@ -63,47 +67,52 @@ class ReleaseGroupTestCase(TestCase):
     # def test_newest_production_is_always_latest(self):
 
 
-# class ReleaseAPITests(APITestCase):
-#     fixtures = ["sample_user.json", "release_data.json"]
+class ReleaseAPITests(APITestCase):
+    fixtures = ["sample_user.json", "release_data.json"]
 
-#     def setUp(self):  # Set up data for the whole TestCase
-#         self.package_key = "basic"  # the name of the test package
-#         self.site = Site.objects.get_current()
+    def setUp(self):  # Set up data for the whole TestCase
+        self.package_key = "basic"  # the name of the test package
+        self.site = Site.objects.get_current()
 
-#         # an average user for testing permissions
-#         self.sampleuser = User.objects.get(username="sampleuser")
+        # an average user for testing permissions
+        self.sampleuser = User.objects.get(username="sampleuser")
 
-#         # a developer user for testing permissions
-#         self.devuser = User.objects.get(username="devuser")
+        # a developer user for testing permissions
+        self.devuser = User.objects.get(username="devuser")
 
-#         # a release user for testing API permissions
-#         self.releaseuser = User.objects.get(username="releaseuser")
+        # a release user for testing API permissions
+        self.releaseuser = User.objects.get(username="releaseuser")
 
-#         # On site ID 1, the current release is 1.0
-#         self.current_release = Release.objects.get(pk=1)
-#         self.beta_release = Release.objects.get(pk=3)
+        # On site ID 1, the current release is 1.0
+        self.current_release = Release.objects.get(pk=1)
+        self.beta_release = Release.objects.get(pk=3)
 
-# def test_releaseuser_can_not_delete_release(self):
+        self.create_release_url = "/api/v1/releases/create/"
 
-#     def test_releaseuser_create_release(self):
-#         """
-#         Ensure we can create a new release object.
-#         """
-#         data = {
-#             "package": "test_package",
-#             "version": "1.1",
-#             "release_date": "2024-05-22T12:00:00Z",
-#             "status": Status.RELEASED,
-#             "release_notes": "Added new features.",
-#             "files": {"css": ["css/styles.css"]},
-#             "signature": "signature123",
-#         }
-#         response = self.client.post(self.create_release_url, data, format="json")
-#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-#         self.assertEqual(Release.objects.count(), 2)
-#         self.assertEqual(
-#             Release.objects.get(version="1.1").release_notes, "Added new features."
-#         )
+    # def test_releaseuser_can_not_delete_release(self):
+
+    def test_releaseuser_create_release(self):
+        """
+        Ensure we can create a new release object.
+        """
+        data = {
+            "package": "test_package",
+            "version": "v1.1",
+            "release_date": "2024-05-22T12:00:00Z",
+            "status": Status.RELEASED,
+            "release_notes": "Added new features.",
+            "files": {"css": ["css/styles.css"]},
+            "signature": "signature123",
+        }
+        # Set the user to the release user
+        self.client.force_authenticate(user=self.releaseuser)
+        response = self.client.post(self.create_release_url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Release.objects.count(), 4)
+        self.assertEqual(
+            Release.objects.get(version="v1.1").release_notes, "Added new features."
+        )
+
 
 #     def test_releaseuser_update_release(self):
 #         """
