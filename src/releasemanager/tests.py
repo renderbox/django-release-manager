95d86d1ffcfd65a6b4322232d59c46619c075894
Grant Viklund <grantv@blackmesaventures.com>
2024-05-24 12:27:48 -0400

    Cleaning up tests

--- src/releasemanager/tests.py
+++ src/releasemanager/tests.py
@@ -22,7 +22,7 @@ User = get_user_model()
 
 
 class ReleaseGroupTestCase(TestCase):
-    fixtures = ["sample_user.json", "release_data.json"]  # ,
+    fixtures = ["sample_user.json", "release_data.json"]
 
     def setUp(self):  # Set up data for the whole TestCase
         self.package_key = "basic"  # the name of the test package
@@ -56,66 +56,36 @@ class ReleaseGroupTestCase(TestCase):
         )
         self.assertEqual(accessible_release, self.beta_release)
 
-    # def test_sampleuser_has_permission_to_view_beta_on_site(self):
-    #     # Test that the user has the correct permissions
-    #     self.assertTrue(self.sampleuser.has_perm("releasemanager.can_view_releases"))
+    # def test_devuser_has_permission_for_specific_package_version(self):
 
-    # def test_get_accessible_releases_no_group_match(self):
-    #     # Create a release that does not match user's group
-    #     Release.objects.create(
-    #         package=self.package_key,
-    #         status=Status.TESTING,
-    #         version="3.0",
-    #         release_date=timezone.now(),
-    #         sites=[self.site],
-    #     )
-    #     # User should still fall back to the latest released version
-    #     accessible_releases = Release.objects.get_accessible_releases(
-    #         self.sampleuser, self.package_key
-    #     )
-    #     self.assertIn(self.release1, list(accessible_releases))
-    #     self.assertNotIn(self.release2, list(accessible_releases))
+    # def test_sampleuser_fallback_to_general_release(self):
 
-    # def test_fallback_to_general_release(self):
-    #     # Test that it falls back to the latest general release
-    #     new_user = User.objects.create_user(username="newuser", password="12345")
-    #     accessible_release = Release.objects.get_accessible_release(
-    #         new_user, self.package_key
-    #     )
-    #     self.assertEqual(accessible_release, self.release1)
+    # def test_newest_production_is_always_latest(self):
 
 
 # class ReleaseAPITests(APITestCase):
-#     def setUp(self):
-#         # Create a user
-#         self.sampleuser = User.objects.create_user(
-#             username="testuser", password="testpass123"
-#         )
-#         self.client = APIClient()
-#         self.client.force_authenticate(user=self.sampleuser)
+#     fixtures = ["sample_user.json", "release_data.json"]
 
-#         # Set up package data in settings
-#         settings.RM_PACKAGES = {"test_package": {"name": "Test Package"}}
+#     def setUp(self):  # Set up data for the whole TestCase
+#         self.package_key = "basic"  # the name of the test package
+#         self.site = Site.objects.get_current()
 
-#         # URL for creating releases
-#         self.create_release_url = reverse("api_create_release")
+#         # an average user for testing permissions
+#         self.sampleuser = User.objects.get(username="sampleuser")
 
-#         # URL for updating files in a release
-#         self.update_files_url = lambda pk: reverse(
-#             "api_update_files", kwargs={"pk": pk}
-#         )
+#         # a developer user for testing permissions
+#         self.devuser = User.objects.get(username="devuser")
 
-#         # Create a release to be updated
-#         self.release = Release.objects.create(
-#             package="test_package",
-#             version="1.0",
-#             release_date="2024-05-21T12:00:00Z",
-#             status=Status.RELEASED,
-#             release_notes="Initial release.",
-#             files={},
-#         )
+#         # a release user for testing API permissions
+#         self.releaseuser = User.objects.get(username="releaseuser")
 
-#     def test_create_release(self):
+#         # On site ID 1, the current release is 1.0
+#         self.current_release = Release.objects.get(pk=1)
+#         self.beta_release = Release.objects.get(pk=3)
+
+# def test_releaseuser_can_not_delete_release(self):
+
+#     def test_releaseuser_create_release(self):
 #         """
 #         Ensure we can create a new release object.
 #         """
@@ -135,7 +105,7 @@ class ReleaseGroupTestCase(TestCase):
 #             Release.objects.get(version="1.1").release_notes, "Added new features."
 #         )
 
-#     def test_update_release_files(self):
+#     def test_releaseuser_update_release(self):
 #         """
 #         Ensure we can add files to the 'files' field of a release.
 #         """
