ee381e46589f7df0766b3824d1b38ebddb49d3c9
Grant Viklund <grant.viklund@gmail.com>
2024-06-07 11:33:45 -0400

    Added configurability to define a different base URL for packages handled by the release manager.

--- src/releasemanager/templatetags/release_template_tags.py
+++ src/releasemanager/templatetags/release_template_tags.py
@@ -1,13 +1,7 @@
 from django import template
-from django.contrib.sites.models import Site
-from django.db.models import Q
-from django.utils import timezone
-from django.conf import settings
-
-from releasemanager.models import Release
+from releasemanager.settings import RM_URL
 
 register = template.Library()
-
 
 @register.inclusion_tag("releasemanager/release_template.html")
 def release_package(package, file_group):
@@ -22,7 +16,7 @@ def release_package(package, file_group):
 
             # if the file does not start with "http" or "/", assume it needs to have the STATIC_URL prepended
             if not file["file_path"].startswith("http") and not file["file_path"].startswith("/"):
-                    file["file_path"] = f"{settings.STATIC_URL}{file['file_path']}"
+                    file["file_path"] = f"{RM_URL}{file['file_path']}"
 
             files.append(file)
 
